7f23a7af0c72fe29e1437ef978ef781307222406
marceloarmando <mcuellarr98@gmail.com>
2020-05-11 14:47:29 -0500

    commit 1

---
+++ Lab07/Lab07/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab07
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item12.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new TextToSpeechDemo());
+
+            };
+            /*Item13.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new IBattery());
+
+            };
+            Item14.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new CodeQR());
+
+            };*/
+        }
+    }
+}
